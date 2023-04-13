0b8ec5f15f5bdb83587d8e2a6e7b344e8e1515f9
Rikas <rikasrkf@gmail.com>
2023-04-14 01:29:41 +0530

    implemented Dashboard template

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
 import { themeSettings } from "./theme";
 import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -14,9 +15,9 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
-          <Navbar />
+            <Navbar />
             <Routes>
-              <Route path="/" element={<div>dashboard page</div>} />
+              <Route path="/" element={<Dashboard />} />
               <Route path="/predictions" element={<div>prediction page</div>} />
             </Routes>
           </Box>
