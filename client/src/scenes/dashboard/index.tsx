acb6e51ea8a0ba7c6308ef6b433124dda54fc312
Rikas <rikasrkf@gmail.com>
2023-04-14 02:58:43 +0530

    Redux Toolkit Setup and Configuration

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,11 +1,8 @@
-import DashboardBox from "@/components/DashboardBox";
 import { Box, useMediaQuery } from "@mui/material";
 
 import Row1 from "./Row1";
 import Row2 from "./Row2";
 import Row3 from "./Row3";
-
-type Props = {};
 
 const gridTemplateLargeScreens = `
   "a b c"
@@ -53,7 +50,7 @@ const gridTemplateSmallScreens = `
   "j"
 `;
 
-const Dashboard = (props: Props) => {
+const Dashboard = () => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
   return (
     <Box
